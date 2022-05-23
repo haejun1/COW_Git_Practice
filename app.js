4283f9549cc306e25b7bddee66b0a12726e02f69
haejun1 <a01027833227@gmail.com>
2022-05-23 19:00:08 +0900

    충돌 해결:

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const user = "해준";
 const branch = "sub";
-<<<<<<< HEAD
-const branch2 = "main";
-=======
 
-const branch2 = "sub에서 만들어서 main에 넣겠다."
->>>>>>> sub
+const branch2= "sub에서 만들어서 main에 넣겠다."
 
 const branch3 = "main에서 만듭니다."
